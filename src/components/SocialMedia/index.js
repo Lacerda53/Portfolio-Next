b2e7ef747a7704d9bcbc2ee872dd57e70b681be0
Guilherme Andrade Lacerda <guilherme.lacerda@itpac_arn.local>
2021-06-24 11:02:18 -0300

    comment whatsapp of contacts

--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -7,16 +7,28 @@ export function SocialMedia() {
       <div data-aos="fade-up">
         <h1 className="title">REDES SOCIAIS</h1>
         <div>
-          <a target="_blank" rel="noopener noreferrer" href="https://wa.me/55">
+          {/* <a target="_blank" rel="noopener noreferrer" href="https://wa.me/5594992643424">
             <FaWhatsapp size={30} />
-          </a>
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/lacerda53">
+          </a> */}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/lacerda53"
+          >
             <FaGithub size={30} />
           </a>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/lacerda53/">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/lacerda53/"
+          >
             <FaLinkedin size={30} />
           </a>
-          <a target="_blank" rel="noopener noreferrer" href="https://lacerda53.medium.com/">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://lacerda53.medium.com/"
+          >
             <FaMedium size={30} />
           </a>
         </div>
